8b2403f3d8637f4607e2e01489fef07237667255
Shumyla <shumyla.fahd@hotmail.com>
2020-11-15 16:40:31 +0000

    add playback functionality

--- src/RedQueenRace/index.js
+++ src/RedQueenRace/index.js
@@ -1,8 +1,12 @@
 import useWebAnimations from '@wellyshen/use-web-animations';
-import React from 'react'
+import React,{ useEffect } from 'react'
 import './styles.css'
 
 function RedQueenRace() {
+
+  let playbackRateRQ = 1;
+ let  playbackRateBG = 0;
+
   /* Background animations */
   const sceneryFrames = [
     { transform: "translateX(100%)" },
@@ -12,19 +16,14 @@ function RedQueenRace() {
   const sceneryTimingBackground = {
     duration: 36000,
     iterations: Infinity,
+    playbackRate: playbackRateBG,
   };
 
   const sceneryTimingForeground = {
     duration: 12000,
     iterations: Infinity,
+    playbackRate: playbackRateBG,
   };
-
-  //   const background1 = document.getElementById('background1');
-  //   const background2 = document.getElementById('background2');
-
-  //   const background1Movement = background1.animate(
-  //   sceneryFrames, sceneryTimingBackground);
-  //   background1Movement.currentTime = background1Movement.effect.timing.duration / 2;
 
   const background1Movement = useWebAnimations({
     keyframes: sceneryFrames,
@@ -46,82 +45,76 @@ function RedQueenRace() {
     timing: sceneryTimingForeground,
   });
 
-  // const background2Movement = background2.animate(
-  //   sceneryFrames, sceneryTimingBackground)const
-  //   const foreground1 = document.getElementById('foreground1');
-  //   const foreground2 = document.getElementById('foreground2');
-
-  //   constforeground1Movement = foreground1.animate(
-  //   sceneryFrames, sceneryTimingForeground);
-  //   foreground1Movement.currentTime = foreground1Movement.effect.timing.duration / 2;
-
-  //   const foreground2Movement = foreground2.animate(
-  //   sceneryFrames, sceneryTimingForeground);
-
   const spriteFrames = [
     { transform: "translateY(0)" },
     { transform: "translateY(-100%)" },
   ];
 
-//   const redQueen_alice_sprite = document.getElementById(
-//     "red-queen_and_alice_sprite"
-//   );
-
-const redQueen_alice_sprite = useWebAnimations({
-    keyframes: spriteFrames,
-    timing: {
+  const aliceSprite = {
         easing: "steps(7, end)",
         direction: "reverse",
         duration: 600,
         playbackRate: 1,
-        iterations: Infinity,}
+        iterations: Infinity,
+
+  }
+
+const redQueen_alice = useWebAnimations({
+    keyframes: spriteFrames,
+    timing: aliceSprite,
 
 })
 
-//   const redQueen_alice = redQueen_alice_sprite.animate(spriteFrames, {
-//     easing: "steps(7, end)",
-//     direction: "reverse",
-//     duration: 600,
-//     playbackRate: 1,
-//     iterations: Infinity,
-//   });
+console.log(redQueen_alice.getAnimation())
 
-//   /* Alice tires so easily! 
-//     Every so many seconds, reduce their playback rate so they slow a little. 
-//   */
-//   const sceneries = [
-//     foreground1Movement,
-//     foreground2Movement,
-//     background1Movement,
-//     background2Movement,
-//   ];
+useEffect(() => {
 
-//   const adjustBackgroundPlayback = function () {
-//     if (redQueen_alice.playbackRate < 0.8) {
-//       sceneries.forEach(function (anim) {
-//         anim.playbackRate = (redQueen_alice.playbackRate / 2) * -1;
-//       });
-//     } else if (redQueen_alice.playbackRate > 1.2) {
-//       sceneries.forEach(function (anim) {
-//         anim.playbackRate = redQueen_alice.playbackRate / 2;
-//       });
-//     } else {
-//       sceneries.forEach(function (anim) {
-//         anim.playbackRate = 0;
-//       });
-//     }
-//   };
-//   adjustBackgroundPlayback();
+  const fganimation = foreground1Movement.getAnimation();
+    fganimation.currentTime = fganimation.effect.getTiming().duration / 2;
+
+    const bganimation = background1Movement.getAnimation();
+    bganimation.currentTime = bganimation.effect.getTiming().duration / 2;
+
+    setInterval( () => {
+      /* Set decay */
+      if (playbackRateRQ > 0.4) {   
+          playbackRateRQ *= 0.9;
+        redQueen_alice.getAnimation().playbackRate = playbackRateRQ
+      }  
+      adjustBackgroundPlayback();
+    }, 3000)
+
+      document.addEventListener("click", () => {
+    playbackRateRQ *= 1.1
+       redQueen_alice.getAnimation().playbackRate = playbackRateRQ;
+       adjustBackgroundPlayback();
+  })
+    }, playbackRateRQ
+)
+
+  const adjustBackgroundPlayback = () => {
+    if (playbackRateRQ < 0.8) {
+      playbackRateBG = (playbackRateRQ / 2) * -1;
+    } else if (playbackRateRQ > 1.2) {
+        playbackRateBG = playbackRateRQ / 2;
+      
+    } else {
+        playbackRateBG = 0;
+      };
+      
+    
+    foreground1Movement.getAnimation().playbackRate = playbackRateBG
+    foreground2Movement.getAnimation().playbackRate = playbackRateBG
+    background1Movement.getAnimation().playbackRate = playbackRateBG
+    background2Movement.getAnimation().playbackRate = playbackRateBG
+      };
+
+
+  //adjustBackgroundPlayback();
 
   /* If Alice and the Red Queen are running at a speed of 1, the background doesn't move. */
 //   /* But if they fall under 1, the background slides backwards */
-//   setInterval(function () {
-//     /* Set decay */
-//     if (redQueen_alice.playbackRate > 0.4) {
-//       redQueen_alice.playbackRate *= 0.9;
-//     }
-//     adjustBackgroundPlayback();
-//   }, 3000);
+ 
 
 //   const goFaster = function () {
 //     /* But you can speed them up by giving the screen a click or a tap. */
@@ -134,80 +127,80 @@ const redQueen_alice_sprite = useWebAnimations({
 
   return (
     <div>
-      <div class='wrapper'>
-        <div class='sky'></div>
-        <div class='earth'>
+      <div className='wrapper'>
+        <div className='sky'></div>
+        <div className='earth'>
           <div id='red-queen_and_alice'>
             <img
               id='red-queen_and_alice_sprite'
               src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png'
-              srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x'
+              srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x'
               alt='Alice and the Red Queen running to stay in place.'
-              ref={redQueen_alice_sprite.ref}
+              ref={redQueen_alice.ref}
             />
           </div>
         </div>
 
-        <div class='scenery' id='foreground1' ref={foreground1Movement.ref}>
+        <div className='scenery' id='foreground1' ref={foreground1Movement.ref}>
           <img
             id='palm3'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x'
             alt=' '
           />
         </div>
-        <div class='scenery' id='foreground2' ref={foreground2Movement.ref}>
+        <div className='scenery' id='foreground2' ref={foreground2Movement.ref}>
           <img
             id='bush'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x'
             alt=' '
           />
           <img
             id='w_rook_upright'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x'
             alt=' '
           />
         </div>
-        <div class='scenery' id='background1' ref={background1Movement.ref}>
+        <div className='scenery' id='background1' ref={background1Movement.ref}>
           <img
             id='r_pawn_upright'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x'
             alt=' '
           />
           <img
             id='w_rook'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x'
             alt=' '
           />
           <img
             id='palm1'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x'
             alt=' '
           />
         </div>
-        <div class='scenery' id='background2' ref={background2Movement.ref}>
+        <div className='scenery' id='background2' ref={background2Movement.ref}>
           <img
             id='r_pawn'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x'
             alt=' '
           />
 
           <img
             id='r_knight'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x'
             alt=' '
           />
           <img
             id='palm2'
             src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png'
-            srcset='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x'
+            srcSet='https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x'
             alt=' '
           />
         </div>
